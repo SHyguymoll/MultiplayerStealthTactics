8cf610d86d7a25dbe495e64aa05958d24e6b1170
SHyguymoll <shymollet@gmail.com>
2024-05-01 20:06:54 -0700

    further expansion on agent code

--- scripts/agent.gd
+++ scripts/agent.gd
@@ -1,20 +1,65 @@
+class_name Agent
 extends Node2D
 
-var view_dist : int
-var view_width_step : int
-var view_direction : int
-var eye_strength : int
+signal action_completed
+signal action_interrupted
+signal agent_died
 
-var hearing_radius : int
+var view_dist : float #length of vision cone
+var view_arc : float #arc of vision cone
+var view_direction : float #radian rotation of view in relation to body rotation
+var eye_strength : float #multiplier applied to vision cone in relation to view target distance
 
-var movement_dist : int
+var hearing_dist : int #distance of furthest possibly heard audio event
 
-var camo_level : int
+var movement_dist : int #distance of furthest possible movement in tiles per turn
+var movement_speed : float #maximum movement speed per turn
 
-var held_items : Array[GameItem]
-var held_weapons : Array[GameWeapon]
+var camo_level : int #bounded from 0 to 100
 
-var selected_item : int
-var selected_weapon : int
+var weapon_accuracy : float #bounded from 0.00 to 1.00
 
-func select_item
+var held_items : Array[GameItem] = [null] #max length should be 5, including no item (null)
+var held_weapons : Array[GameWeapon] = [null] #max length should be 4, including no item (null)
+
+var selected_item : int = 0 #index for item
+var selected_weapon : int = 0 #index for weapon
+
+@onready var _animate : AnimationPlayer = $AnimationPlayer
+
+enum GameActions {
+	GO_STAND, GO_CROUCH, GO_PRONE,
+	MOVE_TO_POS, SNEAK_TO_POS, PARANOID_SNEAK_TO_POS,
+	LOOK_AROUND,
+	CHANGE_ITEM, CHANGE_WEAPON,
+	PICK_UP_ITEM, PICK_UP_WEAPON,
+	USE_ITEM, USE_WEAPON, RELOAD_WEAPON,
+	HALT,
+}
+var queued_action : GameActions
+
+enum States {
+	STAND, CROUCH, PRONE,
+	RUN, SNEAK, CROUCH_WALK, CROUCH_SNEAK, CRAWL,
+	PARANOID_WALK,
+	USING_ITEM, USING_WEAPON, RELOADING_WEAPON,
+	STUNNED, DEAD,
+}
+var state : States = States.STAND
+
+var target_position : Vector2
+var target_direction : float
+
+var target_camo_level : int
+
+func perform_action():
+	match queued_action:
+		GameActions.GO_STAND:
+			match state:
+				States.PRONE, States.CRAWL:
+					_animate.queue("CrouchFromProne")
+					_animate.queue("StandFromCrouch")
+					_animate.queue("Idle")
+				States.CROUCH, States.CROUCH_SNEAK, States.CROUCH_WALK:
+					_animate.queue("StandFromCrouch")
+					_animate.queue("Idle")
