4767a6c38dde50583eec13127dab8245b28cfef9
SHyguymoll <shymollet@gmail.com>
2024-05-10 17:23:10 -0700

    quick info element stuff

---
+++ scripts/hud_agent_small.gd
@@ -0,0 +1,8 @@
+extends Control
+var state := {
+	active = preload("res://assets/sprites/hud_agent_small/active.png"),
+	stunned = preload("res://assets/sprites/hud_agent_small/stunned.png"),
+	asleep = preload("res://assets/sprites/hud_agent_small/asleep.png"),
+	dead = preload("res://assets/sprites/hud_agent_small/dead.png"),
+	unknown = preload("res://assets/sprites/hud_agent_small/unknown.png"),
+}
