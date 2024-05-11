4e0d8b80d674e2e443efaa210e47cd40c4d17ac3
SHyguymoll <shymollet@gmail.com>
2024-05-11 00:24:41 -0700

    expanded hud_agent_small to have all equips
    also moved big images to "state" folder

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -3,16 +3,17 @@ extends Node3D
 
 var peer = ENetMultiplayerPeer.new()
 @export var agent_scene : PackedScene
+var hud_agent_small_scene = preload("res://scenes/hud_agent_small.tscn")
 
-var server_agents : Array[Agent]
-var client_agents : Array[Agent]
+var server_agents : Dictionary
+var client_agents : Dictionary
 
 var selected_agent : Agent = null
 
 func _ready():
 	# Preconfigure game.
-	server_agents = []
-	client_agents = []
+	server_agents = {}
+	client_agents = {}
 	#Lobby.player_loaded.rpc_id(1) # Tell the server that this peer has loaded.
 
 # Called only on the server.
@@ -36,9 +37,9 @@ func ping():
 @rpc("authority", "call_local", "reliable")
 func spawn_agent(player_id, agent_stats):
 	var new_agent = Agent.new()
-	new_agent.agent_selected.connect(_show_agent_state_actions)
+	new_agent.agent_selected.connect(_hud_agent_details_actions)
 	pass
 
-func _show_agent_state_actions(agent : Agent):
+func _hud_agent_details_actions(agent : Agent):
 
 	pass
--- scripts/hud_agent_small.gd
+++ scripts/hud_agent_small.gd
@@ -1,8 +1,42 @@
 extends Control
 var state := {
-	active = preload("res://assets/sprites/hud_agent_small/active.png"),
-	stunned = preload("res://assets/sprites/hud_agent_small/stunned.png"),
-	asleep = preload("res://assets/sprites/hud_agent_small/asleep.png"),
-	dead = preload("res://assets/sprites/hud_agent_small/dead.png"),
-	unknown = preload("res://assets/sprites/hud_agent_small/unknown.png"),
+	active = preload("res://assets/sprites/hud_agent_small/state/active.png"),
+	stunned = preload("res://assets/sprites/hud_agent_small/state/stunned.png"),
+	asleep = preload("res://assets/sprites/hud_agent_small/state/asleep.png"),
+	dead = preload("res://assets/sprites/hud_agent_small/state/dead.png"),
+	unknown = preload("res://assets/sprites/hud_agent_small/state/unknown.png"),
 }
+
+var weapon := {
+	fist = preload("res://assets/sprites/hud_agent_small/weapons/fist.png"),
+	pistol = preload("res://assets/sprites/hud_agent_small/weapons/pistol.png"),
+	rifle = preload("res://assets/sprites/hud_agent_small/weapons/rifle.png"),
+	shotgun = preload("res://assets/sprites/hud_agent_small/weapons/shotgun.png"),
+	grenade_smoke = preload("res://assets/sprites/hud_agent_small/weapons/grenade_smoke.png"),
+	grenade_frag = preload("res://assets/sprites/hud_agent_small/weapons/grenade_frag.png"),
+	noise_maker = preload("res://assets/sprites/hud_agent_small/weapons/noise_maker.png"),
+	middle_flag = preload("res://assets/sprites/hud_agent_small/weapons/flag.png"),
+	enemy_flag = preload("res://assets/sprites/hud_agent_small/weapons/flag.png"),
+}
+
+var item := {
+	none = preload("res://assets/sprites/hud_agent_small/items/none.png"),
+	body_armor = preload("res://assets/sprites/hud_agent_small/items/body_armor.png"),
+	fake_death = preload("res://assets/sprites/hud_agent_small/items/fake_death_pill.png"),
+	box = preload("res://assets/sprites/hud_agent_small/items/box.png"),
+	cigar = preload("res://assets/sprites/hud_agent_small/items/cigar.png"),
+	reflex_enhancer = preload("res://assets/sprites/hud_agent_small/items/reflex_pill.png"),
+	analyzer = preload("res://assets/sprites/hud_agent_small/items/analyzer.png"),
+}
+
+
+func update_state(new_state):
+	($AgentState as TextureRect).texture = state[new_state]
+
+
+func update_weapon(new_weapon):
+	pass
+
+
+func update_item(new_weapon):
+	pass
