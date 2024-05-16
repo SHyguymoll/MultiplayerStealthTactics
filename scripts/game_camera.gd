156f5c8ba93baadbf6be00833c71c6cc63c3ec94
SHyguymoll <shymollet@gmail.com>
2024-05-16 00:10:01 -0700

    basic camera stuff
    plan to incorporate zoom levels later, also funny explosion shake button

--- scripts/game_camera.gd
+++ scripts/game_camera.gd
@@ -1,2 +1,40 @@
 extends Camera3D
 
+var drag_enabled := false
+var last_position := Vector2.ZERO
+var final_position := Vector2.ZERO
+var cutscene_mode := false
+var sensitivity := 5.5
+var quickness := 0.3
+
+func _ready() -> void:
+	position = Vector3(0, 5, 0)
+	h_offset = 0
+	v_offset = 0
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_MIDDLE:
+			print("PRESSED" if event.pressed else "UNPRESSED")
+			drag_enabled = event.pressed
+			if event.pressed:
+				last_position = event.position
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			explode(2, 10)
+	elif event is InputEventMouseMotion and drag_enabled and not cutscene_mode:
+		final_position += (last_position - event.position)
+		last_position = event.position
+
+func _process(delta: float) -> void:
+	position.x = lerpf(position.x, (final_position.x * sensitivity/get_viewport().size.x), quickness)
+	position.z = lerpf(position.z, (final_position.y * sensitivity/get_viewport().size.y), quickness)
+
+	#$/root/Game/Label.text = str(position, "\n", last_position, "\n", final_position, "\n", h_offset, "\n", v_offset)
+# (mouse_offset.x/get_viewport().size.x)
+func explode(force: float, shakes : int):
+	for shake in range(1, shakes+1):
+		h_offset = (randf() - 0.5)*force*(shakes/shake)
+		v_offset = (randf() - 0.5)*force*(shakes/shake)
+		await get_tree().create_timer(0.01).timeout
+	h_offset = 0
+	v_offset = 0
