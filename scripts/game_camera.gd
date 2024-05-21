1d923f9ccdde56cae25daeab9973a191e7c98faf
SHyguymoll <shymollet@gmail.com>
2024-05-21 15:55:33 -0700

    GameCamera now has a raycast used for move. indi.

--- scripts/game_camera.gd
+++ scripts/game_camera.gd
@@ -1,3 +1,4 @@
+class_name GameCamera
 extends Camera3D
 
 var drag_enabled := false
@@ -8,11 +9,13 @@ var custscene_camera_pos := Vector3.ZERO
 var sensitivity := 10.5
 var quickness := 0.3
 var fov_target := 75.0
+var ground_height : float = 0
 
 func _ready() -> void:
 	position = Vector3(0, 15, 0)
 	h_offset = 0
 	v_offset = 0
+
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
@@ -27,6 +30,7 @@ func _input(event: InputEvent) -> void:
 		final_position += (last_position - event.position)
 		last_position = event.position
 
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("fov_change"):
 		match fov_target:
@@ -36,12 +40,16 @@ func _process(delta: float) -> void:
 				fov_target = 20.0
 			_:
 				fov_target = 75.0
-	position.x = lerpf(position.x, (final_position.x * sensitivity/get_viewport().size.x), quickness)
-	position.z = lerpf(position.z, (final_position.y * sensitivity/get_viewport().size.y), quickness)
+	position = position.lerp(Vector3(
+			(final_position.x * sensitivity/get_viewport().size.x),
+			15,
+			(final_position.y * sensitivity/get_viewport().size.y)
+	), quickness)
+	ground_height = ($RayCast3D as RayCast3D).get_collision_point().y
+	$Label.text = "POS: " + str(Vector3(position.x, ground_height, position.z))
 	fov = lerpf(fov, fov_target, quickness)
 
-	#$/root/Game/Label.text = str(position, "\n", last_position, "\n", final_position, "\n", h_offset, "\n", v_offset)
-# (mouse_offset.x/get_viewport().size.x)
+
 func explode(force: float, shakes : int):
 	for shake in range(1, shakes+1):
 		h_offset = (randf() - 0.5)*force*(shakes/shake)
