745a6f6d6523785d25734b388eb22d8cd4d4abcd
SHyguymoll <shymollet@gmail.com>
2024-05-21 17:19:26 -0700

    movement indicators can now be placed
    need to work on removing them now

--- scripts/game_movement_indicator.gd
+++ scripts/game_movement_indicator.gd
@@ -3,10 +3,14 @@ extends AnimatedSprite3D
 signal indicator_placed(indicator)
 const CLOSENESS := 2.0
 
+@onready var _game_camera : GameCamera = $"../../World/Camera3D"
+@onready var _ray : RayCast3D = $RayCast3D
 var referenced_agent : Agent
 var ind_set := false
 
 var flat_position : Vector2
+var ray_position : Vector3
+var position_valid : bool
 
 func _ready() -> void:
 	match referenced_agent.queued_action[1]:
@@ -31,16 +35,44 @@ func _game_step(delta):
 		play("movement_end_fail")
 		return
 
+
 func _on_animation_changed() -> void:
 	if not ind_set:
 		return
 	await animation_finished
 	queue_free()
 
-func _process(delta: float) -> void:
+
+func _check_position() -> bool:
+	# height check
+	if global_position.y > 1:
+		return false
+	# sightline check
+	_ray.global_position = referenced_agent.global_position + Vector3.UP * 0.5
+	_ray.target_position = (global_position - referenced_agent.global_position) + Vector3.UP * 0.5
+	_ray.target_position.y = maxf(_ray.target_position.y, 0.5)
+	_ray.force_raycast_update()
+	if _ray.get_collider():
+		return false
+	return true
+
+
+func _physics_process(delta: float) -> void:
 	if not ind_set:
-		flat_position = get_viewport().get_mouse_position()
-		position = Vector3(flat_position.x, 10, flat_position.y)
+		flat_position.x = _game_camera.position.x
+		flat_position.y = _game_camera.position.z
+		ray_position = Vector3(flat_position.x, _game_camera.ground_height + 0.5, flat_position.y)
+		#distance clamp
+		var ref_ag_move_dist = referenced_agent.movement_dist
+		var ray_to_ag = ray_position - referenced_agent.global_position
+		if ray_to_ag.length() > ref_ag_move_dist:
+			var col_norm = ray_to_ag / ray_to_ag.length()
+			ray_position = referenced_agent.global_position + (col_norm * ref_ag_move_dist)
+		global_position = ray_position
+		position_valid = _check_position()
+	modulate = Color.WHITE if position_valid else Color.RED
+
+
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
