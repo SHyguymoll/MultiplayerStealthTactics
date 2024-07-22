ab319a47efba5d8340a1aae666ef71dae5550697
SHyguymoll <shymollet@gmail.com>
2024-07-21 23:54:07 -0700

    trimming down game_map.gd

--- scripts/game_map.gd
+++ scripts/game_map.gd
@@ -22,22 +22,3 @@ enum Objectives {
 
 @export var server_exfiltrate_zone : Area3D
 @export var client_exfiltrate_zone : Area3D
-
-func _ready() -> void:
-	server_exfiltrate_zone.area_entered.connect(_server_agent_entered_exfil)
-	server_exfiltrate_zone.area_exited.connect(_server_agent_exited_exfil)
-	client_exfiltrate_zone.area_entered.connect(_client_agent_entered_exfil)
-	client_exfiltrate_zone.area_exited.connect(_client_agent_exited_exfil)
-
-
-func _server_agent_entered_exfil(agent_area : Area3D):
-	var agent : Agent = agent_area.get_parent()
-
-func _server_agent_exited_exfil(agent_area : Area3D):
-	var agent : Agent = agent_area.get_parent()
-
-func _client_agent_entered_exfil(agent_area : Area3D):
-	var agent : Agent = agent_area.get_parent()
-
-func _client_agent_exited_exfil(agent_area : Area3D):
-	var agent : Agent = agent_area.get_parent()
