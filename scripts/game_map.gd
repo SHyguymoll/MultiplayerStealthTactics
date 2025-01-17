9631afd6c1120989febd1b8bd7a7c53a3af45129
SHyguymoll <shymollet@gmail.com>
2025-01-16 22:43:26 -0800

    replaced bodged node3ds with spawnpoint scene
    this uses tool too, how fancy!
    also incidentally fixed debug icon positions while I was at it lol

--- scripts/game_map.gd
+++ scripts/game_map.gd
@@ -2,15 +2,15 @@ class_name GameMap
 extends Node3D
 
 # spawns must comfortably support 4 agents
-# agents will be spawned at node3d position (but made flush with floor) with node3d y rotation
-@export var agent_spawn_server_1 : Node3D
-@export var agent_spawn_server_2 : Node3D
-@export var agent_spawn_server_3 : Node3D
-@export var agent_spawn_server_4 : Node3D
-@export var agent_spawn_client_1 : Node3D
-@export var agent_spawn_client_2 : Node3D
-@export var agent_spawn_client_3 : Node3D
-@export var agent_spawn_client_4 : Node3D
+# agents will be spawned at spawnpoint position (but made flush with floor via raycast) with y rotation
+@export var agent_spawn_server_1 : SpawnPoint
+@export var agent_spawn_server_2 : SpawnPoint
+@export var agent_spawn_server_3 : SpawnPoint
+@export var agent_spawn_server_4 : SpawnPoint
+@export var agent_spawn_client_1 : SpawnPoint
+@export var agent_spawn_client_2 : SpawnPoint
+@export var agent_spawn_client_3 : SpawnPoint
+@export var agent_spawn_client_4 : SpawnPoint
 
 enum Objectives {
 	CAPTURE_ENEMY_FLAG,
@@ -24,7 +24,7 @@ enum Objectives {
 @export var client_exfiltrate_zone : Area3D
 
 func _ready():
-	for spawn in $Spawns.get_children() as Array[Sprite3D]:
-		spawn.get_child(0).visible = false
+	for spawn in $Spawns.get_children() as Array[SpawnPoint]:
+		spawn.debug_icon.visible = false
 	$map_basic_nav.visible = false
 	$map_basic.visible = true
---
+++ scripts/spawn_point.gd
@@ -0,0 +1,20 @@
+@tool
+class_name SpawnPoint
+extends Node3D
+
+const SERVER_SPAWN_ICON = preload("res://assets/sprites/game_popups/debug_agspawn_server.png")
+const CLIENT_SPAWN_ICON = preload("res://assets/sprites/game_popups/debug_agspawn_client.png")
+
+@onready var debug_icon : Sprite3D = $DebugIcon
+
+@export var server_true_client_false : bool
+
+func get_spawn_point():
+	return ($RayCast3D as RayCast3D).get_collision_point()
+
+func _ready():
+	debug_icon.texture = SERVER_SPAWN_ICON if server_true_client_false else CLIENT_SPAWN_ICON
+
+func _process(delta: float) -> void:
+	if Engine.is_editor_hint():
+		debug_icon.texture = SERVER_SPAWN_ICON if server_true_client_false else CLIENT_SPAWN_ICON
