e3ee15a6737043277ad1be0a094675bf7e004a40
SHyguymoll <shymollet@gmail.com>
2025-01-25 21:31:43 -0800

    fixed agent spawning

--- scripts/spawn_point.gd
+++ scripts/spawn_point.gd
@@ -10,7 +10,7 @@ const CLIENT_SPAWN_ICON = preload("res://assets/sprites/game_popups/debug_agspaw
 @export var server_true_client_false : bool
 
 func get_spawn_point():
-	return ($RayCast3D as RayCast3D).get_collision_point()
+	return ($RayCast3D as RayCast3D).get_collision_point() + global_position
 
 func _ready():
 	debug_icon.texture = SERVER_SPAWN_ICON if server_true_client_false else CLIENT_SPAWN_ICON
