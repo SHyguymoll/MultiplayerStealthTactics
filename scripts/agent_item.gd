0aba547f76bd10e629727b462d56c0f483b2b2d4
SHyguymoll <shymollet@gmail.com>
2025-06-16 21:29:03 -0700

    doc comments for each variable
    copied from the comments in load_agent_old

--- scripts/agent_item.gd
+++ scripts/agent_item.gd
@@ -1,17 +1,30 @@
 class_name AgentItem
 extends Resource
 
+## The name of the agent.
 @export var name : String
+## The number of missions that this agent has succeeded at and survived.
 @export var mission_count : int
+## The life force of the agent. This value is strictly between 1 and 4 health points, inclusive.
 @export var health : int
+## The energy of the agent. This value is strictly between 3 and 8 stun health points, inclusive.
 @export var stun_health : int
+## How far the agent can see, between 2.00 and 3.50 meters.
 @export var view_dist : float
+## The peripheral vision of the agent, between 0.80 and 1.20 meters.
 @export var view_across : float
+## The visual acuity of the agent when an element/agent is within its sights, between 0.30 and 0.60.[br]
+## This value acts as an exponent when calculating sight chances. The formula is included [here](https://www.desmos.com/calculator/azk19m9ik3)
 @export var eye_strength : float
+## The radius of sounds the agent can pick up, between 0.80 and 1.65 meters.
 @export var hearing_dist : float
+## How far the agent can move in a single turn, between 4.00 and 9.00 meters.
 @export var movement_dist : float
+## How fast the agent moves in a single turn, between 1.25 and 3.00 meters/tick.
 @export var movement_speed : float
+## The items held by the agent. Maxes out at 2.
 @export var held_items : Array[String]
+## The weapons equipped by the agent. Maxes out at 2.
 @export var held_weapons : Array[String]
 
 func serialize_agent():
